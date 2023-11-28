e001f177152f448cadc35a24458e644379b87a12
jeremy-data-scientist <137361135+jeremy-data-scientist@users.noreply.github.com>
2023-11-28 03:39:24 +0000

    a

--- js/script.js
+++ js/script.js
@@ -101,7 +101,7 @@ function generateExamples() {
     for (let i = 1; i <= 4; i++) {
         exampleInputs.innerHTML += `
             <div class="example-container">
-                <div class="highlighted-text" id="highlighted-text${i}"> test</div>
+                <div class="highlighted-text" id="highlighted-text${i}" contenteditable="true">Example text here...</div>
                 <button onclick="editText(${i})">Edit</button>
                 <div class="capture-group" id="capture-group${i}"></div>
             </div>
